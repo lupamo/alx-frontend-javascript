85e7e47018ba7530bfb8938a3dda88ab694c6a71
arnoldlupamo@gmail.com <arnoldlupamo@gmail.com>
2024-07-23 00:15:57 +0300

    linter1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,6 +1,6 @@
 export default function taskBlock(trueOrFalse) {
-  const task = !!trueOrFalse;
-  const task2 = !!trueOrFalse;
+  const task = trueOrFalse ? true : false;
+  const task2 = trueOrFalse ? true : false;
 
   return [task, task2];
 }

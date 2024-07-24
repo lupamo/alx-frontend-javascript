f6b8f42a223490c3b6403f3172b67d624cf481aa
arnoldlupamo@gmail.com <arnoldlupamo@gmail.com>
2024-07-24 23:36:55 +0300

    upload user credentials

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,19 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+	const [photo, user] = await Promise.all([
+		uploadPhoto('filename'),
+		createUser()
+    ]);
+    return {
+		photo: photo,
+		user: user
+	};
+  } catch (error) {
+    return {
+		photo: null,
+		user: null
+	};
+  }
+}

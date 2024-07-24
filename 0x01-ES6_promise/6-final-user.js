ee6c9006226602123266b9d906a62b634c17a809
arnoldlupamo@gmail.com <arnoldlupamo@gmail.com>
2024-07-24 23:14:53 +0300

    race

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,14 @@
+import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled(
+    [signUpPromise, uploadPromise],
+  ).then((results) => results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : result.reason,
+  })));
+}

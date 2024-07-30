9842a5939d13b357e244d4404eedb2abfb33b6da
arnoldlupamo@gmail.com <arnoldlupamo@gmail.com>
2024-07-30 15:35:09 +0300

    new set

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,5 +1,7 @@
 export default function setFromArray(arr) {
-  const setter =  new Set();
-  setter.add(arr);
+  const setter = new Set();
+  for (let i = 0; i < arr.length; i += 1) {
+    setter.add(arr[i]);
+  }
   return setter;
 }

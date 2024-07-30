79f992226c2c1db302888e006498e84ac53b1272
arnoldlupamo@gmail.com <arnoldlupamo@gmail.com>
2024-07-30 22:00:36 +0300

    mapping

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,12 @@
+export default function updateUniqueItems(mapper) {
+  if (!(mapper instanceof Map)) {
+	throw new Error('Cannot process');
+  }
+
+  for (const [key, value] of mapper) {
+    if (value === 1) {
+      mapper.set(key, 100);
+    }
+  }
+  return mapper;
+}

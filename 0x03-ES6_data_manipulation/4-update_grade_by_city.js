e6a2acda310a375bd73f1c9a02d41ecacff9cd40
arnoldlupamo@gmail.com <arnoldlupamo@gmail.com>
2024-07-30 14:37:49 +0300

    tab error

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,11 +2,10 @@ export default function updateStudentGradeByCity(listStudents, city, newGrade) {
   return listStudents
     .filter((student) => student.location === city)
     .map((student) => {
-	  const gradeObj = newGrade.find((grade) => grade.studentId === student.id);
-	  return {
+      const gradeObj = newGrade.find((grade) => grade.studentId === student.id);
+      return {
         ...student,
         grade: gradeObj ? gradeObj.grade : 'N/A',
-
-	  };
+      };
     });
 }
